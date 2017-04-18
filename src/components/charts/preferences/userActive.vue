46b566de15a874572b87fc770a5f727e726f04b3
shipf <shipf3@lenovo.com>
2017-04-18 11:37:23 +0800

    fixed user active

--- src/components/charts/preferences/userActive.vue
+++ src/components/charts/preferences/userActive.vue
@@ -1,11 +1,31 @@
 <template>
-<div id="user-active-chart-container" style="width:100%;height:400px"></div>
+    <div class="user-active-chart-wrapper">
+        <div id="user-active-chart-container1" style="width:100%;height:150px"></div>
+        <div id="user-active-chart-container2" style="width:100%;height:150px"></div>
+        <div id="user-active-chart-container3" style="width:100%;height:150px"></div>
+        <div id="user-active-chart-container4" style="width:100%;height:150px"></div>
+    </div>
 </template>
+
 <script type="text/babel">
     import $ from 'jquery'
     import localforage from 'localforage'
     import { mapGetters } from 'vuex'
     var echarts = require('echarts');
+     var xAxisData = [];
+            var data1 = [];
+            var data2 = [];
+            var data3 = [];
+            var data4 = [];
+
+            for (var i = 0; i < 9; i++) {
+                xAxisData.push('2017-02-0' + (i+1));
+                data1.push((Math.random() * 100).toFixed(0));
+                data2.push((Math.random() * 50).toFixed(0));
+                data3.push((Math.random() * 200).toFixed(0));
+                data4.push((Math.random() * 300).toFixed(0));
+            }
+
     export default {
           name: 'userActive',
         data() {
@@ -25,24 +45,13 @@
             //this.$store.dispatch('getHomeInfo')
         },
         methods: {
-            drawGraph (id) {
+            drawGraph (id,legend,seriesData,xAxisShow,lineColor) {
             this.chart=echarts.init(document.getElementById(id));
             // 皮肤添加同一般使用方式  
             this.chart.showLoading();
-            var xAxisData = [];
-            var data1 = [];
-            var data2 = [];
-            var data3 = [];
-            var data4 = [];
-
-            for (var i = 0; i < 10; i++) {
-                xAxisData.push('2017-09-0' + (i+1));
-                data1.push((Math.random() * 100).toFixed(2));
-                data2.push((Math.random() * 50).toFixed(2));
-                data3.push((Math.random() * 200).toFixed(2));
-                data4.push((Math.random() + 300).toFixed(2));
-            }
-
+            let legendData=[];
+            legendData.push(legend);
+           
             var itemStyle = {
                 normal: {
                 }
@@ -50,7 +59,12 @@
 
             let option = {
                 title: {
-                    text: ''
+                    text: legend,
+                    textStyle:{
+                        color:'#333',
+                        fontSize:'12'
+                    },
+                    bottom:'20%'
                 },
                 tooltip : {
                     trigger: 'axis',
@@ -62,7 +76,9 @@
                     }
                 },
                 legend: {
-                    data:['App','论坛','商城','设备']
+                    show:false,
+                    data:legendData
+                    //['App','论坛','商城','设备']
                 },
             
                 grid: {
@@ -73,47 +89,52 @@
                 },
                 xAxis : [
                     {
+                        show:xAxisShow,
                         type : 'category',
-                        boundaryGap : false,
-                        data : xAxisData
+                        boundaryGap : true,
+                        data : xAxisData,
+                         axisLabel:{
+                         show:true,
+                         interval:0
+                        }
                     }
                 ],
                 yAxis : [
                     {
+                        show:false,
                         type : 'value',
                         boundaryGap:true,
                         axisTick: {
-                          show:false,
+                          show:false,  
                           alignWithLabel:true,
                           inside:false
-                        }
+                        },
+                        axisLine:{
+                            show:true
+                        },
                     }
                 ],
                 series : [
                     {
-                        name:'App',
+                        name:legend,
                         type:'line',
                         stack: '总量',
-                       
-                        data:data1
+                         itemStyle: {
+                        normal: {
+                            color: lineColor,
+                            lineStyle: {        // 系列级个性化折线样式
+                                width: 2,
+                            },
+                        label: {
+                        show: true,
+                        formatter: "{c}",
+                        textStyle: {
+                            color: '#000'
+                            }
                     },
-                    {
-                        name:'论坛',
-                        type:'line',
-                        stack: '总量',
-                        data:data2
+                        }
                     },
-                    {
-                        name:'商城',
-                        type:'line',
-                        stack: '总量',
-                        data:data3
-                    },
-                    {
-                        name:'设备',
-                        type:'line',
-                        stack: '总量',
-                        data:data4
+                    data:seriesData
                     }
                 ]
             };
@@ -123,8 +144,26 @@
         },
         mounted() {
            this.$nextTick(()=> {  
-                this.drawGraph('user-active-chart-container');  
+                this.drawGraph('user-active-chart-container1','App',data1,false,'#9ACD32');  
+                this.drawGraph('user-active-chart-container2','论坛',data2,false,'#9370DB');
+                this.drawGraph('user-active-chart-container3','商城',data3,false,'#8E388E');  
+                this.drawGraph('user-active-chart-container4','设备',data4,true,'#98FB98');  
             }) 
         }
     }
 </script>
+<style lang="less" scoped>
+    .user-active-chart-wrapper{
+        > div{
+            &:before{
+                    content: '';
+    border-left: 1px solid #121212;
+    height: 150px;
+    padding-left: 20px;
+    position: absolute;
+    left: 59px;
+    /* margin-left: 50px; */
+            }
+        }
+    }
+</style>
--- src/components/user/userSearch.vue
+++ src/components/user/userSearch.vue
@@ -1,32 +1,50 @@
 <template>
-    <section class="user-search" v-loading.fullscreen.lock="fullscreenLoading"
-    element-loading-text="拼命加载中">
-                      <h2>用户查询</h2>
-                      <p>支持手机号/LenovoID/Mail/IMEI/SN/ServiceUserID</p>
-                      <div class="search-block">
-                      <el-form :model="userValidateForm" :inline="true" ref="userValidateForm"  >
-                        <el-form-item  prop="userName" :rules="[
+    <section class="user-search" v-loading.fullscreen.lock="fullscreenLoading" element-loading-text="拼命加载中">
+        <h2>用户查询</h2>
+        <p>支持手机号/LenovoID/Mail/IMEI/SN/ServiceUserID</p>
+        <div class="search-block">
+            <el-form :model="userValidateForm" :inline="true" ref="userValidateForm">
+               <!--  <el-form-item prop="userName" :rules="[
       { required: true, message: '用户不能为空'}
     ]">
-                            <el-input icon="search" :on-icon-click="handleIconClick" v-model.trim="userValidateForm.userName" placeholder="查询用户" auto-complete="off"></el-input>
-                        </el-form-item>
-                    </el-form>
-                    </div>
-                    </section>
+                    <el-input icon="search" :on-icon-click="serchClick" v-model.trim="userValidateForm.userName" placeholder="查询用户" auto-complete="off"></el-input>
+                </el-form-item> -->
+                <el-form-item>
+                    <el-autocomplete popper-class="my-autocomplete" v-model="userValidateForm.userName" :fetch-suggestions="querySearch" custom-item="my-item-zh" placeholder="请输入内容" @select="handleSelect" icon="search" :on-icon-click="serchClick"></el-autocomplete>
+                </el-form-item>
+            </el-form>
+        </div>
+    </section>
 </template>
+
 
 
 <script type="text/babel">
     import $ from 'jquery'
     import localforage from 'localforage'
+    import Vue from 'vue'
     import { mapGetters } from 'vuex'
+
+    Vue.component('my-item-zh', {
+      functional: true,
+      render: function (h, ctx) {
+        var item = ctx.props.item;
+        return h('li', ctx.data, [
+          h('div', { attrs: { class: 'name' } }, [item.value])
+        ]);
+      },
+      props: {
+        item: { type: Object, required: true }
+      }
+  });
     export default {
         name: 'userSearch',
         data() {
             return {
                userValidateForm: {
                   userName: localStorage.getItem('customer-profile-username')?localStorage.getItem('customer-profile-username'):''
-               }
+               },
+                restaurants: []
               }
         },
         computed: {
@@ -40,9 +58,10 @@
           //this.$store.dispatch('getHomeInfo');
         },
         mounted() {
+           this.restaurants = this.loadAll();
         },
         methods: {
-          handleIconClick(){
+          serchClick(){
               const userName = this.userValidateForm.userName.trim();
                 if ( userName.length > 0 ) {
                       //this.fullscreenLoading = true;
@@ -59,8 +78,34 @@
                         });
                         }
               },
-            changeDatas () {
-            }
+          
+             querySearch(queryString, cb) {
+              var restaurants = this.restaurants;
+              var results = queryString ? restaurants.filter(this.createFilter(queryString)) : restaurants;
+              // 调用 callback 返回建议列表的数据
+              cb(results);
+          },
+          createFilter(queryString) {
+            return (restaurant) => {
+              return (restaurant.value.indexOf(queryString.toLowerCase()) === 0);
+            };
+          },
+          loadAll() {
+            return [
+              { "value": "testLenovoID2" },
+              { "value": "testLenovoID" },
+              { "value": "18911004567" },
+              { "value": "test@163.com" },
+              { "value": "13916053476" },
+              { "value": "admin@lenovo.com"}
+            ];
+          },
+          handleSelect(item) {
+             this.serchClick();             
+          },
+          handleIconClick(ev) {
+            console.log(ev);
+          }
         }
     }
 </script>
@@ -87,4 +132,27 @@
     margin-bottom: 0;
   }
 }
+    .my-autocomplete {
+            li {
+              line-height: normal;
+              padding: 7px;
+              &:hover{
+                background-color: #eaeefb;
+              }
+              .name {
+                text-overflow: ellipsis;
+                overflow: hidden;
+              }
+              .addr {
+                font-size: 12px;
+                color: #b4b4b4;
+              }
+
+              .highlighted .addr {
+                color: #ddd;
+              }
+            }
+          }
+      }
+    }
 </style>
